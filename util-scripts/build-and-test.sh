3ba98f7dfe8893fd858385d808831419b4498dc2
C.D. Clark III <clifton.clark@gmail.com>
2018-03-10 09:52:41 -0600

    teaking the installation.

--- util-scripts/build-and-test.sh
+++ util-scripts/build-and-test.sh
@@ -13,9 +13,51 @@ trap cleanup EXIT
 
 mkdir ${bindir}
 cd ${bindir}
-cmake ..
+cmake .. -DCMAKE_INSTALL_PREFIX=${bindir}/install
 cmake --build .
 cmake --build . --target test
+
+
+
+
+
+
+
+
+# test install
+
+cmake --build . --target install
+
+mkdir app
+cd app
+
+cat << EOF > main.cpp
+#include <iostream>
+#include <libField_version.h>
+#include <Field.hpp>
+
+int main()
+{
+  std::cout << "VERSION: " << libField_VERSION << std::endl;
+  std::cout << "FULL VERSION: " << libField_VERSION_FULL << std::endl;
+
+  Field<double,3> F(10,5,2);
+
+  return 0;
+}
+EOF
+
+cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION 3.1)
+add_executable( main main.cpp )
+find_package( libField REQUIRED )
+target_link_libraries(main libField::Field )
+set_target_properties(main PROPERTIES CXX_STANDARD 14)
+EOF
+
+cmake . -DlibField_DIR=${bindir}/install/cmake/libField
+cmake --build .
+./main
 
 echo "PASSED"
 
