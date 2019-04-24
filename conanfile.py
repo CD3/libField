8fc7363fed979ed94ea4dd6f9ec2aedcb39640d2
C.D. Clark III <clifton.clark@gmail.com>
2019-04-24 10:01:06 -0500

    refactor: reworking conan support
    Removed conan code to build package and moved to providing a
    conan file that can be used to build the project. Plan on creating
    a separate project for building the actual package.

--- conanfile.py
+++ conanfile.py
@@ -2,36 +2,12 @@ from conans import ConanFile, CMake, tools
 import os
 
 class libFieldConan(ConanFile):
-    name = "libField"
-    version = "master"
-    license = "MIT"
-    author = "CD Clark III clifton.clark@gmail.com"
-    url = "https://github.com/CD3/conan-libField"
-    homepage = "https://github.com/CD3/libField"
-    description = "A C++ library for storing and working with field data."
-    topics = ("C++", "Physics")
-
     generators = "cmake", "virtualenv"
     requires = 'boost/1.69.0@conan/stable'
     build_requires = 'cmake_installer/3.13.0@conan/stable'
 
-    exports_sources = 'CMakeLists.txt','cmake/*','src/*', 'LICENSE.md', 'README.md'
-
 
     def build(self):
-      tools.replace_in_file('CMakeLists.txt',
-                            'project(libField)',
-                            'project(libField)\nset(STANDALONE OFF)')
-
       cmake = CMake(self)
       cmake.configure()
-
       cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-        
-    def package_info(self):
-        self.env_info.libField_DIR = os.path.join(self.package_folder, "cmake")
-
