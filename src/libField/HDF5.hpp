8d38739aa3d36446ee4cf16949a512547dffaf1a
C.D. Clark III <clifton.clark@gmail.com>
2019-09-11 06:44:37 -0500

    feat: added functions for reading/writing to specified path in HDF5 files.
    Can now read/write fields from/to specified paths in a HDF5 file. For
    example:
    
    hdf5write("myData.h5", "Experiment 1/Trial 2/T vs. t", T);

--- src/libField/HDF5.hpp
+++ src/libField/HDF5.hpp
@@ -1,6 +1,8 @@
 #include <H5Cpp.h>
 #include <exception>
 #include <string>
+#include <iostream>
+#include <boost/algorithm/string.hpp>
 #include "./Field.hpp"
 
 namespace detail {
@@ -29,6 +31,12 @@ const H5::PredType& get_hdf5_dtype_for_type<float>()
 {
   return H5::PredType::NATIVE_FLOAT;
 };
+
+template<typename CT>
+bool path_exists( const CT& container, std::string group )
+{
+  return H5Lexists(container.getId(), group.c_str(), H5P_DEFAULT);
+}
 
 }
 
@@ -97,6 +105,54 @@ void hdf5write(std::string name, const Field<FT, N, CT>& f, decltype(H5F_ACC_TRU
 }
 
 template <typename FT, size_t N, typename CT>
+void hdf5write(std::string name, std::vector<std::string> path, const Field<FT, N, CT>& f, decltype(H5F_ACC_TRUNC) acc = H5F_ACC_RDWR)
+{
+  H5::H5File file(name.c_str(), acc);
+  H5::Group group = file.openGroup("/");
+
+  for( auto &elem: path )
+  {
+    if( detail::path_exists(group,elem) )
+      group = group.openGroup(elem);
+    else
+      group = group.createGroup(elem);
+  }
+  hdf5write( group, f);
+  file.close();
+}
+
+/**
+ * Writes a field to a specified group in an HDF5 files.
+ *
+ * Each coordinate axis and the field data are written
+ * to separate datasets. Axes are written to datasets named
+ * 'axis {i}', where '{i}' is the axis number (zero offset).
+ * The field data is written to a dataset named 'field'.
+ *
+ * For example, a 2D, 5x10 field will be written to three datasets.
+ *
+ * "axis 0" will contain the 5 coordinates of the first dimension.
+ * "axis 1" will contain the 10 coordinates of the second dimension.
+ * "field" will contain the 50 elements of the field.
+ *
+ */
+template <typename FT, size_t N, typename CT>
+void hdf5write(std::string name, std::string path, const Field<FT, N, CT>& f, decltype(H5F_ACC_TRUNC) acc = H5F_ACC_TRUNC)
+{
+  auto is_slash = [](char c){return c=='/';};
+  boost::trim_if(path, is_slash);
+  std::vector<std::string> elems;
+  boost::split(elems, path, is_slash);
+  hdf5write(name,elems,f,acc);
+}
+
+/*
+ * Reads an HDF5 dataset into a field. The dataset is assumed to be an N-D array
+ * and is read directly into the field data. Coordinates are set to integer indices.
+ *
+ * This function is useful for reading data written by some other application into a field.
+ */
+template <typename FT, size_t N, typename CT>
 void hdf5read(H5::DataSet& dset, Field<FT, N, CT>& f)
 {
   auto dspace = dset.getSpace();
@@ -126,6 +182,13 @@ void hdf5read(H5::DataSet& dset, Field<FT, N, CT>& f)
 
 }
 
+
+/*
+ * Reads a field from an HDF5 container (either a group, or a file).
+ *
+ * @param container  the HDF5 container (file or group) to read from.
+ * @param f the field to read data into.
+ */
 template <typename ST, typename FT, size_t N, typename CT>
 auto hdf5read(ST& container, Field<FT, N, CT>& f) -> decltype(container.createGroup(std::string()),void())
 {
@@ -147,10 +210,66 @@ auto hdf5read(ST& container, Field<FT, N, CT>& f) -> decltype(container.createGr
   dset.read(f.data(), detail::get_hdf5_dtype_for_type<FT>());
 
   for (int i = 0; i < N; ++i) {
-    auto dset = container.openDataSet(("axis " + std::to_string(i)).c_str());
+    std::string dsetname{"axis " + std::to_string(i)};
+    auto dset = container.openDataSet(dsetname.c_str());
+    auto dspace = dset.getSpace();
+    if (dspace.getSimpleExtentNdims() != 1)
+      throw std::runtime_error("Cannot read axis data from '"+dsetname+"'. It does not contain a 1D array.");
+    hsize_t ddims[1];
+    dspace.getSimpleExtentDims(ddims);
+    if (ddims[0] != f.size(i) )
+      throw std::runtime_error("Cannot read axis data from '"+dsetname+"'. Size ("+std::to_string(ddims[0])+") does not match size expected from field data ("+std::to_string(f.size(i))+").");
+
     dset.read(f.getAxis(i).data(), detail::get_hdf5_dtype_for_type<CT>());
   }
 }
+
+/*
+ * Reads a field from an HDF5 group specified by a list of path elements in an HDF5 container (either a group, or a file).
+ *
+ * @param container  the HDF5 container (file or group) to read from.
+ * @param path a list of path elements to locate the group.
+ * @param f the field to read data into.
+ *
+ * This function can be used to read fields from a file that contains multiple fields. It assumes
+ * that the data is foratted the same way that it will be written. Each coordinate axis is read from
+ * a dataset named "axis <N>", where N is the axis index (zero offset), and the field data is read
+ * from a dataset named "field".
+ */
+template <typename ST, typename FT, size_t N, typename CT>
+auto hdf5read(ST& container, const std::vector<std::string>& path, Field<FT, N, CT>& f) -> decltype(container.createGroup(std::string()),void())
+{
+  H5::Group group = container.openGroup("/");
+  for( auto &elem: path )
+  {
+    group = group.openGroup(elem);
+  }
+  hdf5read(group,f);
+}
+
+/*
+ * Reads a field from an HDF5 group specified by a path in an HDF5 container (either a group, or a file).
+ *
+ * @param container  the HDF5 container (file or group) to read from.
+ * @param path a string representation of a path (i.e. '/path/to/element')
+ * @param f the field to read data into.
+ *
+ * This function can be used to read fields from a file that contains multiple fields. It assumes
+ * that the data is foratted the same way that it will be written. Each coordinate axis is read from
+ * a dataset named "axis <N>", where N is the axis index (zero offset), and the field data is read
+ * from a dataset named "field".
+ */
+template <typename ST, typename FT, size_t N, typename CT>
+auto hdf5read(ST& container, std::string path, Field<FT, N, CT>& f) -> decltype(container.createGroup(std::string()),void())
+{
+  auto is_slash = [](char c){return c=='/';};
+  boost::trim_if(path, is_slash);
+  std::vector<std::string> elems;
+  boost::split(elems, path, is_slash);
+  hdf5read(container,elems,f);
+}
+
+
 
 /**
  * Reads a field from an HdF5 file.
@@ -163,6 +282,26 @@ void hdf5read(std::string name, Field<FT, N, CT>& f)
   H5::H5File file(name.c_str(), H5F_ACC_RDONLY);
   try {
   hdf5read(file, f);
+  } catch( std::runtime_error& e)
+  {
+    throw std::runtime_error("There was an error reading field from '" + name +". "+e.what());
+  }
+
+  file.close();
+}
+
+/**
+ * Reads a field from a specified group in an HdF5 file.
+ *
+ * This function assumes that the file contains a group (which is specified) that
+ * is structured in the way written by hdf5write.
+ */
+template <typename FT, size_t N, typename CT>
+void hdf5read(std::string name, std::string path, Field<FT, N, CT>& f)
+{
+  H5::H5File file(name.c_str(), H5F_ACC_RDONLY);
+  try {
+  hdf5read(file, path, f);
   } catch( std::runtime_error& e)
   {
     throw std::runtime_error("There was an error reading field from '" + name +". "+e.what());
--- testing/CatchTests/HDF5IOTests.cpp
+++ testing/CatchTests/HDF5IOTests.cpp
@@ -118,6 +118,73 @@ TEST_CASE("HDF5 Read and Write")
     }
   }
 
+  SECTION("Reading from Nested Datasets")
+  {
+    Field<double, 1> F(10);
+    F.setCoordinateSystem(Uniform(0, 2));
+    F.set_f([](auto x) { return x[0] * x[0]; });
+
+    H5::H5File file("ManyFields.h5", H5F_ACC_TRUNC);
+    auto       g1 = file.createGroup("Field 1");
+    hdf5write(g1, F);
+    g1.close();
+
+    auto g2 = file.createGroup("Experiment 1").createGroup("Data 1").createGroup("Trial 1");
+    hdf5write(g2, F);
+    g2.close();
+
+    file.close();
+
+    {
+      Field<double, 1> G;
+      H5::H5File file("ManyFields.h5", H5F_ACC_RDONLY);
+      std::vector<std::string> path;
+      path.push_back("Field 1");
+      hdf5read( file, path, G);
+
+      CHECK(G.size() == 10);
+      CHECK(G(0) == 0);
+      CHECK(G(9) == 4);
+      file.close();
+    }
+
+    {
+      Field<double, 1> G;
+      H5::H5File file("ManyFields.h5", H5F_ACC_RDONLY);
+      std::vector<std::string> path(3);
+      path[0] = "Experiment 1";
+      path[1] = "Data 1";
+      path[2] = "Trial 1";
+      hdf5read( file, path, G);
+
+      CHECK(G.size() == 10);
+      CHECK(G(0) == 0);
+      CHECK(G(9) == 4);
+      file.close();
+    }
+
+    {
+      Field<double, 1> G;
+      H5::H5File file("ManyFields.h5", H5F_ACC_RDONLY);
+      hdf5read( file, "/Experiment 1/Data 1/Trial 1", G);
+
+      CHECK(G.size() == 10);
+      CHECK(G(0) == 0);
+      CHECK(G(9) == 4);
+      file.close();
+    }
+
+    {
+      Field<double, 1> G;
+      hdf5read( "ManyFields.h5", "Experiment 1/Data 1/Trial 1/", G);
+
+      CHECK(G.size() == 10);
+      CHECK(G(0) == 0);
+      CHECK(G(9) == 4);
+      file.close();
+    }
+  }
+
   SECTION("Reading other formats")
   {
     {
@@ -207,59 +274,163 @@ TEST_CASE("HDF5 Read and Write")
       Field<double, 1> f;
       auto             dset = file.openDataSet("Small 1D Array");
       hdf5read(dset, f);
-      CHECK( f.size() == 10 );
-      CHECK( f(0) == Approx(0) );
-      CHECK( f(1) == Approx(1) );
-      CHECK( f(9) == Approx(9) );
+      CHECK(f.size() == 10);
+      CHECK(f(0) == Approx(0));
+      CHECK(f(1) == Approx(1));
+      CHECK(f(9) == Approx(9));
 
-      CHECK( f.getAxis(0)[0] == Approx(0) );
-      CHECK( f.getAxis(0)[9] == Approx(9) );
+      CHECK(f.getAxis(0)[0] == Approx(0));
+      CHECK(f.getAxis(0)[9] == Approx(9));
     }
     SECTION("2D Field")
     {
       Field<double, 2> f;
       auto             dset = file.openDataSet("Small 2D Array");
       hdf5read(dset, f);
-      CHECK( f.size() == 15 );
-      CHECK( f.size(0) == 3 );
-      CHECK( f.size(1) == 5 );
-      CHECK( f(0,0) == Approx(0) );
-      CHECK( f(1,0) == Approx(1) );
-      CHECK( f(0,1) == Approx(1) );
-      CHECK( f(1,1) == Approx(2) );
-      CHECK( f(2,4) == Approx(6) );
+      CHECK(f.size() == 15);
+      CHECK(f.size(0) == 3);
+      CHECK(f.size(1) == 5);
+      CHECK(f(0, 0) == Approx(0));
+      CHECK(f(1, 0) == Approx(1));
+      CHECK(f(0, 1) == Approx(1));
+      CHECK(f(1, 1) == Approx(2));
+      CHECK(f(2, 4) == Approx(6));
 
-      CHECK( f.getAxis(0)[0] == Approx(0) );
-      CHECK( f.getAxis(0)[2] == Approx(2) );
-      CHECK( f.getAxis(1)[0] == Approx(0) );
-      CHECK( f.getAxis(1)[4] == Approx(4) );
-
+      CHECK(f.getAxis(0)[0] == Approx(0));
+      CHECK(f.getAxis(0)[2] == Approx(2));
+      CHECK(f.getAxis(1)[0] == Approx(0));
+      CHECK(f.getAxis(1)[4] == Approx(4));
     }
 
     SECTION("3D Field")
     {
       Field<float, 3> f;
-      auto             dset = file.openDataSet("Small 3D Array");
+      auto            dset = file.openDataSet("Small 3D Array");
       hdf5read(dset, f);
-      CHECK( f.size() == 24 );
-      CHECK( f.size(0) == 2 );
-      CHECK( f.size(1) == 3 );
-      CHECK( f.size(2) == 4 );
-      CHECK( f(0,0,0) == Approx(0) );
-      CHECK( f(1,0,0) == Approx(1) );
-      CHECK( f(0,1,0) == Approx(1) );
-      CHECK( f(1,1,1) == Approx(3) );
-      CHECK( f(1,2,3) == Approx(6) );
+      CHECK(f.size() == 24);
+      CHECK(f.size(0) == 2);
+      CHECK(f.size(1) == 3);
+      CHECK(f.size(2) == 4);
+      CHECK(f(0, 0, 0) == Approx(0));
+      CHECK(f(1, 0, 0) == Approx(1));
+      CHECK(f(0, 1, 0) == Approx(1));
+      CHECK(f(1, 1, 1) == Approx(3));
+      CHECK(f(1, 2, 3) == Approx(6));
 
-      CHECK( f.getAxis(0)[0] == Approx(0) );
-      CHECK( f.getAxis(0)[1] == Approx(1) );
-      CHECK( f.getAxis(1)[0] == Approx(0) );
-      CHECK( f.getAxis(1)[2] == Approx(2) );
-      CHECK( f.getAxis(2)[0] == Approx(0) );
-      CHECK( f.getAxis(2)[3] == Approx(3) );
-
+      CHECK(f.getAxis(0)[0] == Approx(0));
+      CHECK(f.getAxis(0)[1] == Approx(1));
+      CHECK(f.getAxis(1)[0] == Approx(0));
+      CHECK(f.getAxis(1)[2] == Approx(2));
+      CHECK(f.getAxis(2)[0] == Approx(0));
+      CHECK(f.getAxis(2)[3] == Approx(3));
     }
     file.close();
+  }
+
+  SECTION("Read Errors")
+  {
+    H5::H5File file("ReadErrors.h5", H5F_ACC_TRUNC);
+
+    {
+      auto   group = file.createGroup("Mismatched Axis Size");
+      double data[3][5];
+      for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 5; ++j) data[i][j] = i + j;
+      hsize_t dims[2];
+      dims[0] = 3;
+      dims[1] = 5;
+
+      {
+        H5::DataSpace dspace(2, dims);
+        auto          dset =
+            group.createDataSet("field", H5::PredType::NATIVE_DOUBLE, dspace);
+        dset.write(data, H5::PredType::NATIVE_DOUBLE);
+        dset.close();
+      }
+
+      {
+        dims[0] = 2;
+        H5::DataSpace dspace(1, &dims[0]);
+        auto          dset =
+            group.createDataSet("axis 0", H5::PredType::NATIVE_DOUBLE, dspace);
+        dset.write(dims, H5::PredType::NATIVE_DOUBLE);
+        dset.close();
+      }
+
+      {
+        H5::DataSpace dspace(1, &dims[1]);
+        auto          dset =
+            group.createDataSet("axis 1", H5::PredType::NATIVE_DOUBLE, dspace);
+        dset.write(&data[0], H5::PredType::NATIVE_DOUBLE);
+        dset.close();
+      }
+
+      group.close();
+    }
+
+    file.close();
+
+    SECTION("Axis size mismatch")
+    {
+      Field<float, 2> f;
+      H5::H5File      ifile("ReadErrors.h5", H5F_ACC_RDONLY);
+      auto            group = ifile.openGroup("Mismatched Axis Size");
+      CHECK_THROWS_WITH(
+          hdf5read(group, f),
+          Catch::Equals("Cannot read axis data from 'axis 0'. Size (2) does "
+                        "not match size expected from field data (3)."));
+    }
+  }
+
+  SECTION("Writing multiple fields to the same file")
+  {
+    Field<double,1> T(10);
+    T.setCoordinateSystem(Uniform(0,2));
+    Field<float,1> U(20);
+    U.setCoordinateSystem(Uniform(0,1));
+
+    hdf5write( "MultipleFieldWrite.h5", "1D/Tvst", T, H5F_ACC_TRUNC );
+    hdf5write( "MultipleFieldWrite.h5", "1D/Uvst", U, H5F_ACC_RDWR );
+
+    Field<double,2> E(10,10);
+    E.setCoordinateSystem(Uniform(0,2),Uniform(0,3));
+    Field<float,2> F(20,10);
+    F.setCoordinateSystem(Uniform(-1,1),Uniform(0,2));
+
+    hdf5write( "MultipleFieldWrite.h5", "2D/Evst", E, H5F_ACC_RDWR );
+    hdf5write( "MultipleFieldWrite.h5", "2D/Fvst", F, H5F_ACC_RDWR );
+
+    Field<double,1> _1DF;
+    Field<float,2> _2DF;
+
+    hdf5read("MultipleFieldWrite.h5", "1D/Tvst", _1DF);
+    CHECK(_1DF.size() == 10);
+    CHECK(_1DF.getAxis(0)[0] == Approx(0));
+    CHECK(_1DF.getAxis(0)[9] == Approx(2));
+
+    hdf5read("MultipleFieldWrite.h5", "1D/Uvst", _1DF);
+    CHECK(_1DF.size() == 20);
+    CHECK(_1DF.getAxis(0)[0] == Approx(0));
+    CHECK(_1DF.getAxis(0)[19] == Approx(1));
+
+    hdf5read("MultipleFieldWrite.h5", "2D/Evst", _2DF);
+    CHECK(_2DF.size() == 100);
+    CHECK(_2DF.size(0) == 10);
+    CHECK(_2DF.size(1) == 10);
+    CHECK(_2DF.getAxis(0)[0] == Approx(0));
+    CHECK(_2DF.getAxis(0)[9] == Approx(2));
+    CHECK(_2DF.getAxis(1)[0] == Approx(0));
+    CHECK(_2DF.getAxis(1)[9] == Approx(3));
+
+    hdf5read("MultipleFieldWrite.h5", "2D/Fvst", _2DF);
+    CHECK(_2DF.size() == 200);
+    CHECK(_2DF.size(0) == 20);
+    CHECK(_2DF.size(1) == 10);
+    CHECK(_2DF.getAxis(0)[0] == Approx(-1));
+    CHECK(_2DF.getAxis(0)[19] == Approx(1));
+    CHECK(_2DF.getAxis(1)[0] == Approx(0));
+    CHECK(_2DF.getAxis(1)[9] == Approx(2));
+
   }
 }
 #endif
